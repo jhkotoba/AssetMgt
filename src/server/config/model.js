abdee886a4418fb0ea8a1f233772cd3a9d18a740
jhkot <jhkot@DESKTOP-L7T4HDJ>
2022-10-05 23:56:38 +0900

    페이지 이동 수정

--- src/server/config/model.js
+++ src/server/config/model.js
@@ -1,7 +1,21 @@
 const fs = require("fs");
+const logger = require(`${basePath}/config/logger.js`);
 
 exports.modelAndView = function(path){
-    let index = fs.readFileSync(`${public}/view/index.html`).toString();
-    let main = fs.readFileSync(`${public}/view/${path}`).toString();
-    return index.replace("<main>", "<main>" + main);
+    return Promise.all([
+        new Promise((resolve, reject) => {
+            fs.readFile(`${public}/view/index.html`, 'UTF-8', (error, value) => {
+                if(error) reject(error);
+                else resolve(value);
+            })
+        }),
+        new Promise((resolve, reject) => {
+            fs.readFile(`${public}/view/${path}`, 'UTF-8', (error, value) => {
+                if(error) reject(error);
+                else resolve(value);
+            })
+        })
+    ])
+    .then(values => values[0].replace("<main>", "<main>" + values[1]))
+    .catch(error => logger.error(error));
 }
--- src/server/routes/main.js
+++ src/server/routes/main.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const model = require(`${basePath}/config/model.js`);
 
-router.get("/", (request, response) => response.send(model.modelAndView('/main/main.html')));
+router.get("/", async (request, response) => response.send(await model.modelAndView('/main/main.html')));
 
 module.exports = router;
 
