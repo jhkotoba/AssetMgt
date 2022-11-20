59f987208fe09bb9039529e8b1a2f89aff4c06dc
jhkotoba <jhkotoba@gmail.com>
2022-11-20 23:27:36 +0900

    계좌관리 작업

--- src/server/routes/account.js
+++ src/server/routes/account.js
@@ -1,6 +1,12 @@
-const path = require("path");
 const express = require("express");
 const router = express.Router();
+const model = require(`${basePath}/config/model.js`);
+//const accountController = require(`${basePath}/controllers/account/accountController.js`)
 
+// 계좌관리 페이지
+router.get("/account", async (request, response) => response.send(await model.modelAndView('account/account.html')));
+
+// 계좌내역 페이지
+router.get("/record", async (request, response) => response.send(await model.modelAndView('account/record.html')));
 
 module.exports = router;
