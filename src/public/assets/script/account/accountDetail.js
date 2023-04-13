7bf82ec38b489ddeb559e559cccb1a86bbad87a2
jhkotoba <jhkotoba@gmail.com>
2023-04-13 23:00:42 +0900

    계좌관리 작업

---
+++ src/public/assets/script/account/accountDetail.js
@@ -0,0 +1,6 @@
+import { sender } from "/script/common/sender.js";
+import { isEmpty } from "/script/common/validation.js";
+
+window.addEventListener('DOMContentLoaded', function(event){
+    console.log('detail page');
+});
